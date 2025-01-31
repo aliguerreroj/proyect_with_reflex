f8422f4e693a27b88e94cedb455a1d0807f741f9
Ali Guerrero <aliguerreroj@gmail.com>
2025-01-31 18:53:56 -0400

    arreglo de build.sh agregando el back al front  y seguridad de cors

--- rxconfig.py
+++ rxconfig.py
@@ -2,4 +2,9 @@ import reflex as rx
 
 config = rx.Config(
     app_name="link_bio",
+    # api_url="https://proyect-reflex.up.railway.app",
+    cors_allowed_origins = [
+        "http://localhost:3000",
+        "https://proyect-reflex.up.railway.app"
+    ]
 )
